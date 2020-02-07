a90a0a96c18ea828d21475a90826f6a3c33aaec4
sriyegna <sriyegna@users.noreply.github.com>
2020-02-07 10:04:47 -0500

    Added buttons
    First input is not hidden

--- src/app/portfolio-graph/portfolio-graph.component.ts
+++ src/app/portfolio-graph/portfolio-graph.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { StockService } from '../shared/stock.service';
 import { Observable } from 'rxjs';
-import {NgbDate, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+import {NgbDate, NgbCalendar, NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-portfolio-graph',
@@ -59,7 +59,7 @@ export class PortfolioGraphComponent implements OnInit {
   selectedStock = "AMD";
   selectedDays = 5;
 
-  constructor(private service:StockService, calendar: NgbCalendar) {
+  constructor(private service:StockService, private calendar: NgbCalendar, public formatter: NgbDateParserFormatter) {
     this.fromDate = calendar.getNext(calendar.getToday(), 'm', -6);
     this.toDate = calendar.getToday();
    }
@@ -165,6 +165,34 @@ export class PortfolioGraphComponent implements OnInit {
       )
     })
     return observable;
+  }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate) {
+    return date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || date.equals(this.toDate) || this.isInside(date) || this.isHovered(date);
+  }
+
+  validateInput(currentValue: NgbDate, input: string): NgbDate {
+    const parsed = this.formatter.parse(input);
+    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
   }
 
 }
